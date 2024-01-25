fe69ae8b29e4f7429b05f6adc9ddd4282fd28d4e
arihant-jain0 <jainari1208@gmail.com>
2024-01-25 22:09:21 +0530

    add:shadcn ui

---
+++ src/components/Star.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+const Star = () => {
+  return (
+    <div className='flex'>
+        <FontAwesomeIcon
+              icon={faStar}
+              className="h-3"
+              style={{ color: "#2e803a" }}
+            />
+        <FontAwesomeIcon
+              icon={faStar}
+              className="h-3"
+              style={{ color: "#2e803a" }}
+            />
+        <FontAwesomeIcon
+              icon={faStar}
+              className="h-3"
+              style={{ color: "#2e803a" }}
+            />
+        <FontAwesomeIcon
+              icon={faStar}
+              className="h-3"
+              style={{ color: "#2e803a" }}
+            />
+        <FontAwesomeIcon
+              icon={faStar}
+              className="h-3"
+              style={{ color: "#2e803a" }}
+            />
+           
+    </div>
+  )
+}
+
+export default Star
+
