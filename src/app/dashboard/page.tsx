3843cbb9a588956c63c64ca4e25313c673304387
arihant-jain0 <jainari1208@gmail.com>
2024-01-26 15:52:49 +0530

    Update environment variables and add Google authentication provider

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,73 +1,52 @@
-import { Metadata } from "next"
-import Image from "next/image"
+import { Metadata } from "next";
+import Image from "next/image";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from "@/components/ui/tabs"
-import { CalendarDateRangePicker } from "@/components/ui/date-ranege-piker"
-import { MainNav } from "@/components/ui/main-nav"
-import { Overview } from "@/components/ui/overview"
-import { RecentSales } from "@/components/ui/recent-sales"
-import { Search } from "@/components/ui/search"
-import TeamSwitcher from "@/components/ui/team-switcher"
-import { UserNav } from "@/components/ui/user-nav"
+} from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { CalendarDateRangePicker } from "@/components/ui/date-ranege-piker";
+import { MainNav } from "@/components/ui/main-nav";
+import { Overview } from "@/components/ui/overview";
+import { RecentSales } from "@/components/ui/recent-sales";
+import { Search } from "@/components/ui/search";
+import TeamSwitcher from "@/components/ui/team-switcher";
+import { UserNav } from "@/components/ui/user-nav";
 
 export const metadata: Metadata = {
   title: "Dashboard",
   description: "Example dashboard app built using the components.",
-}
+};
 
 export default function DashboardPage() {
   return (
     <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/dashboard-light.png"
-          width={1280}
-          height={866}
-          alt="Dashboard"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/dashboard-dark.png"
-          width={1280}
-          height={866}
-          alt="Dashboard"
-          className="hidden dark:block"
-        />
-      </div>
       <div className="flex flex-col md:flex">
         <div className="border-b">
           <div className="flex h-16 items-center px-4">
             <TeamSwitcher />
-            <MainNav className="mx-6" />
-            <div className="ml-auto flex items-center space-x-4">
+            <MainNav className="mx-6 md:flex hidden" />
+            <div className="ml-auto md:flex hidden items-center space-x-4">
               <Search />
               <UserNav />
             </div>
           </div>
         </div>
-        <div className="flex-1 space-y-4 p-8 pt-6">
+        <div className="flex-1  space-y-4 p-8 pt-6">
           <div className="flex items-center justify-between space-y-2">
             <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
-            <div className="flex items-center space-x-2">
+            <div className="flex items-center space-x-2 md:flex hidden">
               <CalendarDateRangePicker />
               <Button>Download</Button>
             </div>
           </div>
-          <Tabs defaultValue="overview" className="space-y-4">
-            <TabsList>
+          <Tabs defaultValue="overview" className="space-y-4 ">
+            <TabsList className="">
               <TabsTrigger value="overview">Overview</TabsTrigger>
               <TabsTrigger value="analytics" disabled>
                 Analytics
@@ -209,5 +188,5 @@ export default function DashboardPage() {
         </div>
       </div>
     </>
-  )
+  );
 }
