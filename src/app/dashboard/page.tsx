8e04ac9c3d2131aed21fd1022b4854c679bddcae
arihant-jain0 <jainari1208@gmail.com>
2024-01-26 15:56:12 +0530

    Fix flex alignment issue in dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -40,7 +40,7 @@ export default function DashboardPage() {
         <div className="flex-1  space-y-4 p-8 pt-6">
           <div className="flex items-center justify-between space-y-2">
             <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
-            <div className="flex items-center space-x-2 md:flex hidden">
+            <div className=" items-center space-x-2 md:flex hidden">
               <CalendarDateRangePicker />
               <Button>Download</Button>
             </div>
