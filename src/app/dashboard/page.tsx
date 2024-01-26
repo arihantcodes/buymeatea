1d7062704bf3ec6914a5da25e2dee9c645d3e603
arihant-jain0 <jainari1208@gmail.com>
2024-01-26 16:01:20 +0530

    update in code

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -88,7 +88,7 @@ export default function DashboardPage() {
                 <Card>
                   <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                     <CardTitle className="text-sm font-medium">
-                      Subscriptions
+                      Arihant
                     </CardTitle>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
