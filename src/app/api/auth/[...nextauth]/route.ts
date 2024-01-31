2f4b10bdeef6e25bef74744b0d28d3c532baddc7
arihant-jain0 <jainari1208@gmail.com>
2024-01-31 14:46:50 +0530

    Update NextAuth configuration

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,35 +1,29 @@
-import NextAuth from "next-auth/next";
+import NextAuth from "next-auth";
+
+
+
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-
-
 export const authOptions = {
-providers: [
-GithubProvider({
-clientId: process.env.GITHUB_ID ?? "",
-clientSecret: process.env.GITHUB_SECRET ?? "",
-}),
-GoogleProvider({
-clientId: process.env.GOOGLE_ID ?? "",
-clientSecret: process.env.GOOGLE_SECRET ?? "",
-}),
-],
-callbacks: {
-    async redirect(url:any, baseUrl:any) {
-      // Customize the redirect URL after sign-up
-      return '/dashboard/profile/arihanthu';
-    },
-  },
+  providers:[
+  GithubProvider({
+    clientId: process.env.GITHUB_ID || "",
+    clientSecret: process.env.GITHUB_SECRET || "",
+  }),
+  GoogleProvider({
+    clientId: process.env.GOOGLE_ID || "",
+    clientSecret: process.env.GOOGLE_SECRET || "",
+  }),
+]
 }
 
-export const handler = NextAuth({
-    ...authOptions,
-    callbacks: {
-        async redirect(params: { url: string; baseUrl: string }) {
-        
-            return '/dashboard/profile/';
-        },
-    },
-});
+const callbacks = {
+  async redirect(url: string, baseUrl: string) {
+    // Customize the redirect URL after sign-up
+    return '/dashboard/profile/arihanthu';
+  },
+};
 
-export {handler as GET, handler as POST};
+
+
+export default NextAuth(authOptions);
